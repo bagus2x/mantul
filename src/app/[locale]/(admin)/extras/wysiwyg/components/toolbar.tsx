4e2cf9186f3f54c3fdec3df7d0b388b2ed290f18
Tubagus Saifulloh <dutapanggulu@gmail.com>
2024-09-09 16:32:51 +0700

    Bug fixing failed to removeChild

--- src/app/[locale]/(admin)/extras/wysiwyg/components/toolbar.tsx
+++ src/app/[locale]/(admin)/extras/wysiwyg/components/toolbar.tsx
@@ -1,17 +1,18 @@
 import { Editor } from '@tiptap/core'
 import { BubbleMenu } from '@tiptap/react'
-import { Fragment, PropsWithChildren, useCallback, useRef, useState } from 'react'
+import { PinIcon, PinOffIcon } from 'lucide-react'
+import { PropsWithChildren, useCallback, useRef } from 'react'
 
 import { ToolbarHeadingStyle } from '@mantul/app/[locale]/(admin)/extras/wysiwyg/components/toolbar-heading-style'
 import { ToolbarLink } from '@mantul/app/[locale]/(admin)/extras/wysiwyg/components/toolbar-link'
 import { ToolbarListStyle } from '@mantul/app/[locale]/(admin)/extras/wysiwyg/components/toolbar-list-style'
 import { ToolbarTextStyle } from '@mantul/app/[locale]/(admin)/extras/wysiwyg/components/toolbar-text-style'
 import { ToolbarUndoRedo } from '@mantul/app/[locale]/(admin)/extras/wysiwyg/components/toolbar-undo-redo'
-import { Separator } from '@mantul/components/ui/separator'
-import { cn } from '@mantul/libs/utils'
 import { Button } from '@mantul/components/ui/button'
-import { PinIcon, PinOffIcon } from 'lucide-react'
+import { Separator } from '@mantul/components/ui/separator'
 import { useToggle } from '@mantul/hooks/use-toggle'
+import { cn } from '@mantul/libs/utils'
+
 export interface EditorToolbarProps {
   className?: string
   editor: Editor
@@ -25,16 +26,18 @@ export const Toolbar = ({ editor, className }: EditorToolbarProps) => {
     ({ children }: PropsWithChildren) => {
       if (isBubble) {
         return (
-          <BubbleMenu
-            editor={editor}
-            className='w-full max-w-screen-2xl'
-            tippyOptions={{ maxWidth: 765 }}>
-            {children}
-          </BubbleMenu>
+          <div>
+            <BubbleMenu
+              editor={editor}
+              className='w-full max-w-screen-2xl'
+              tippyOptions={{ maxWidth: 765 }}>
+              {children}
+            </BubbleMenu>
+          </div>
         )
       }
 
-      return <div>{children}</div>
+      return <>{children}</>
     },
     [isBubble, editor],
   )
@@ -58,6 +61,7 @@ export const Toolbar = ({ editor, className }: EditorToolbarProps) => {
         <Separator orientation='vertical' className='mx-2 h-full min-h-6' />
         <ToolbarLink editor={editor} />
         <div className='flex-1' />
+        <Separator orientation='vertical' className='mx-2 h-full min-h-6' />
         <Button size='icon' variant='ghost' onClick={toggleBubble}>
           {isBubble ? <PinOffIcon className='size-4' /> : <PinIcon className='size-4' />}
         </Button>
